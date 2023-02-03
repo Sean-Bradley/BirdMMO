8f3f7e8dd836232a11af91473f11bb58b58b7efe
Sean Bradley <seanwasere@gmail.com>
2023-02-03 00:34:16 +0000

    updates

--- src/Bird.jsx
+++ src/Bird.jsx
@@ -4,7 +4,7 @@ import { useFrame } from '@react-three/fiber'
 import useKeyboard from './useKeyboard'
 import { MathUtils, Vector3 } from 'three'
 
-const GRAVITY = 30
+const GRAVITY = 50
 
 export default function Bird() {
   const model = useRef()
@@ -15,6 +15,7 @@ export default function Bird() {
   const keyMap = useKeyboard()
   const velocity = useMemo(() => new Vector3(), [])
   const lookAt = useMemo(() => new Vector3(), [])
+  const startPosition = useMemo(() => new Vector3(-260, 1, 0), [])
 
   useFrame((state, delta) => {
     count.current += delta
@@ -42,18 +43,19 @@ export default function Bird() {
       }
     }
 
-    keyMap['Space'] && (velocity.y = 9)
-    keyMap['KeyR'] && model.current.position.set(-160, 10, 0) && velocity.set(0, 0, 0)
+    keyMap['Space'] && (velocity.y = 10)
+    keyMap['KeyR'] && model.current.position.copy(startPosition) && velocity.set(0, 0, 0)
 
     let damping = Math.exp(-4 * delta) - 1
-    if (model.current.position.y > .65) {
+    if (model.current.position.y > 0.65) {
       velocity.x += delta * 2
       velocity.y -= GRAVITY * delta
-      damping *= 0.1 // small air resistance
+      damping *= 0.066 // gradual speed up
     } else {
+      model.current.position.y = 0.65
       velocity.x = 0
-      velocity.y = 0
-      damping = 0
+      //velocity.y = 0
+      // damping = 0
     }
     velocity.addScaledVector(velocity, damping)
     const deltaPosition = velocity.clone().multiplyScalar(delta)
@@ -73,33 +75,33 @@ export default function Bird() {
   })
 
   return (
-    <group ref={model} dispose={null} position={[-160, 10, 0]}>
+    <group ref={model} dispose={null} position={startPosition}>
       <group ref={frame[0]}>
-        <mesh geometry={nodes.Cube303.geometry} material={materials['Material.003']} position={[-0.25, -0.45, 0]} scale={[0.05, 0.05, 0.13]} />
-        <mesh geometry={nodes.Cube330.geometry} material={materials['Material.002']} position={[0.05, 0.45, 0]} scale={[0.05, 0.05, 0.13]} />
-        <mesh geometry={nodes.Cube332.geometry} material={materials['Material.001']} position={[-0.15, 0.35, 0]} scale={[0.05, 0.05, 0.13]} />
-        <mesh geometry={nodes.Cube344.geometry} material={materials['Material.004']} position={[0.25, 0.05, 0]} scale={[0.05, 0.05, 0.13]} />
-        <mesh geometry={nodes.Cube358.geometry} material={materials['Material.005']} position={[0.55, 0.25, 0]} scale={[0.05, 0.05, 0.13]} />
-        <mesh geometry={nodes.Cube373.geometry} material={materials['Material.006']} position={[0.65, -0.35, 0]} scale={[0.05, 0.05, 0.13]} />
-        <mesh geometry={nodes.Cube429.geometry} material={materials['Material.007']} position={[-0.35, -0.35, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube303.geometry} material={materials['Material.003']} castShadow position={[-0.25, -0.45, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube330.geometry} material={materials['Material.002']} castShadow position={[0.05, 0.45, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube332.geometry} material={materials['Material.001']} castShadow position={[-0.15, 0.35, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube344.geometry} material={materials['Material.004']} castShadow position={[0.25, 0.05, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube358.geometry} material={materials['Material.005']} castShadow position={[0.55, 0.25, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube373.geometry} material={materials['Material.006']} castShadow position={[0.65, -0.35, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube429.geometry} material={materials['Material.007']} castShadow position={[-0.35, -0.35, 0]} scale={[0.05, 0.05, 0.13]} />
       </group>
       <group ref={frame[1]}>
-        <mesh geometry={nodes.Cube008.geometry} material={materials['Material.001']} position={[-0.65, -0.15, 0]} scale={[0.05, 0.05, 0.13]} />
-        <mesh geometry={nodes.Cube031.geometry} material={materials['Material.002']} position={[-0.25, 0.35, 0]} scale={[0.05, 0.05, 0.13]} />
-        <mesh geometry={nodes.Cube039.geometry} material={materials['Material.003']} position={[-0.15, -0.35, 0]} scale={[0.05, 0.05, 0.13]} />
-        <mesh geometry={nodes.Cube048.geometry} material={materials['Material.004']} position={[0.25, 0.05, 0]} scale={[0.05, 0.05, 0.13]} />
-        <mesh geometry={nodes.Cube051.geometry} material={materials['Material.005']} position={[0.15, 0.35, 0]} scale={[0.05, 0.05, 0.13]} />
-        <mesh geometry={nodes.Cube077.geometry} material={materials['Material.006']} position={[0.65, -0.35, 0]} scale={[0.05, 0.05, 0.13]} />
-        <mesh geometry={nodes.Cube095.geometry} material={materials['Material.007']} position={[0.65, 0.05, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube008.geometry} material={materials['Material.001']} castShadow position={[-0.65, -0.15, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube031.geometry} material={materials['Material.002']} castShadow position={[-0.25, 0.35, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube039.geometry} material={materials['Material.003']} castShadow position={[-0.15, -0.35, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube048.geometry} material={materials['Material.004']} castShadow position={[0.25, 0.05, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube051.geometry} material={materials['Material.005']} castShadow position={[0.15, 0.35, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube077.geometry} material={materials['Material.006']} castShadow position={[0.65, -0.35, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube095.geometry} material={materials['Material.007']} castShadow position={[0.65, 0.05, 0]} scale={[0.05, 0.05, 0.13]} />
       </group>
       <group ref={frame[2]}>
-        <mesh geometry={nodes.Cube169.geometry} material={materials['Material.001']} position={[-0.25, 0.25, 0]} scale={[0.05, 0.05, 0.13]} />
-        <mesh geometry={nodes.Cube180.geometry} material={materials['Material.002']} position={[-0.05, 0.45, 0]} scale={[0.05, 0.05, 0.13]} />
-        <mesh geometry={nodes.Cube193.geometry} material={materials['Material.003']} position={[0.05, -0.45, 0]} scale={[0.05, 0.05, 0.13]} />
-        <mesh geometry={nodes.Cube195.geometry} material={materials['Material.004']} position={[0.25, 0.05, 0]} scale={[0.05, 0.05, 0.13]} />
-        <mesh geometry={nodes.Cube207.geometry} material={materials['Material.005']} position={[0.35, 0.45, 0]} scale={[0.05, 0.05, 0.13]} />
-        <mesh geometry={nodes.Cube219.geometry} material={materials['Material.006']} position={[0.15, -0.25, 0]} scale={[0.05, 0.05, 0.13]} />
-        <mesh geometry={nodes.Cube233.geometry} material={materials['Material.007']} position={[0.05, -0.25, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube169.geometry} material={materials['Material.001']} castShadow position={[-0.25, 0.25, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube180.geometry} material={materials['Material.002']} castShadow position={[-0.05, 0.45, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube193.geometry} material={materials['Material.003']} castShadow position={[0.05, -0.45, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube195.geometry} material={materials['Material.004']} castShadow position={[0.25, 0.05, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube207.geometry} material={materials['Material.005']} castShadow position={[0.35, 0.45, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube219.geometry} material={materials['Material.006']} castShadow position={[0.15, -0.25, 0]} scale={[0.05, 0.05, 0.13]} />
+        <mesh geometry={nodes.Cube233.geometry} material={materials['Material.007']} castShadow position={[0.05, -0.25, 0]} scale={[0.05, 0.05, 0.13]} />
       </group>
     </group>
   )
--- src/Pipes.jsx
+++ src/Pipes.jsx
@@ -1,7 +1,12 @@
 import Pipe from './Pipe'
 
-const positions = [...Array(50)].map((_, i) => ({
-  position: [i * 10 - 150, Math.random() * 5 + 3, 0]
+// a seedable RNG. https://gist.github.com/blixt/f17b47c62508be59987b
+const LCG = (s) => (_) => (s = Math.imul(741103597, s) >>> 0) / 2 ** 32
+const nextRandom = LCG(12345678)
+
+const positions = [...Array(51)].map((_, i) => ({
+  // height differences between pipes get gradually more extreme
+  position: [i * 10 - 250, nextRandom(1) * (5 + i / 7.5) + 3, 0]
 }))
 
 export default function Pipes() {
