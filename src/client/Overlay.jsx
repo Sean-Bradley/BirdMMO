b6d52ffbffcdcf0f257ad7e8e7e6210b749a0564
Sean Bradley <seanwasere@gmail.com>
2023-02-06 11:43:01 +0000

    updates

--- src/client/Overlay.jsx
+++ src/client/Overlay.jsx
@@ -5,6 +5,8 @@ import { useFrame } from '@react-three/fiber'
 export default function Overlay({ model, crashed, started, keyMap }) {
   //console.log('updating score')
   const [score, setScore] = useState(0)
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+  const isPortraight = window.matchMedia('(orientation: portrait)').matches
 
   useFrame(() => {
     let s = Math.floor((model.current.position.x - 2) / 10) + 26
@@ -18,33 +20,29 @@ export default function Overlay({ model, crashed, started, keyMap }) {
       <Html>
         <div id="score">{score}</div>
       </Html>
-      <Html>
-        <div className="prompt" style={{ display: crashed ? 'block' : 'none' }}>
-          Press <kbd>R</kbd> to re-start
-        </div>
-      </Html>
-      <Html>
-        <div className="prompt" style={{ display: started ? 'none' : 'block' }}>
-          Press <kbd>Space</kbd> to Start
-        </div>
-      </Html>
-      <MobileButtons keyMap={keyMap} />
+      {!isMobile ? (
+        <Html>
+          <div className="prompt" style={{ display: crashed ? 'block' : 'none' }}>
+            Press <kbd>R</kbd> to re-start
+          </div>
+          <div className="prompt" style={{ display: started ? 'none' : 'block' }}>
+            Press <kbd>Space</kbd> to Start
+          </div>
+        </Html>
+      ) : isPortraight ? (
+        <Html>
+          <div className="prompt">Please use landscape mode</div>
+        </Html>
+      ) : (
+        <Html>
+          <button className="button" id="flapButton" onPointerDown={() => (keyMap['Space'] = true)} onPointerUp={() => (keyMap['Space'] = false)}>
+            Flap
+          </button>
+          <button className="button" id="restartButton" onPointerDown={() => (keyMap['KeyR'] = true)} onPointerUp={() => (keyMap['KeyR'] = false)}>
+            Re-Start
+          </button>
+        </Html>
+      )}
     </Hud>
   )
-}
-
-function MobileButtons({ keyMap }) {
-  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
-  if (isMobile) {
-    return (
-      <Html>
-        <button className="button" id="flapButton" onPointerDown={() => (keyMap['Space'] = true)} onPointerUp={() => (keyMap['Space'] = false)}>
-          Flap
-        </button>
-        <button className="button" id="restartButton" onPointerDown={() => (keyMap['KeyR'] = true)} onPointerUp={() => (keyMap['KeyR'] = false)}>
-          Re-Start
-        </button>
-      </Html>
-    )
-  }
 }
