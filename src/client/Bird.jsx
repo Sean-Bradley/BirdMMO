3d2aed999d9d08d71eba326053bbef454eb444da
Sean Bradley <seanwasere@gmail.com>
2023-07-14 22:51:24 +0100

    updates

--- src/client/Bird.jsx
+++ src/client/Bird.jsx
@@ -1,6 +1,5 @@
 import { useRef, useState, useEffect } from 'react'
 import { useGLTF } from '@react-three/drei'
-import { useFrame } from '@react-three/fiber'
 
 export default function Bird({ crashed = false, color = 'green' }) {
   const frame = [useRef(), useRef(), useRef()]
