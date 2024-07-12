05ef53b56d3abf14a33d3fafe564620d45f6116f
sean bradley <seanwasere@gmail.com>
2024-07-12 11:47:52 +0100

    updates

--- src/client/useKeyboard.jsx
+++ src/client/useKeyboard.jsx
@@ -7,19 +7,11 @@ export default function useKeyboard() {
     const onDocumentKey = (e) => {
       keyMap.current[e.code] = e.type === 'keydown'
     }
-    const preventDefault = (e) => {
-      if (e.code === 'Space') {
-        //stop space causing parent window to scroll when birdmmo viewed in an iframe
-        e.preventDefault()
-      }
-    }
     document.addEventListener('keydown', onDocumentKey)
     document.addEventListener('keyup', onDocumentKey)
-    document.addEventListener('keypress', preventDefault)
     return () => {
       document.removeEventListener('keydown', onDocumentKey)
       document.removeEventListener('keyup', onDocumentKey)
-      document.removeEventListener('keypress', preventDefault)
     }
   })
 
